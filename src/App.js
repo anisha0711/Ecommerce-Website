c50f6744f72f9350897273fa7ce3d5ed9c9fcb64
Anisha <anishamodak07@gmail.com>
2023-07-24 02:37:39 +0530

    commit-3

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 import Home from './features/pages/Home';
@@ -13,11 +13,18 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import Protected from './features/auth/components/Protected';
+import { fetchItemsByUserId } from './features/cart/cartAPI';
+import { fetchItemsByUserIdAsync } from './features/cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectLoggedInUser } from './features/auth/authSlice';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home></Home>,
+    element: <Protected>
+      <Home></Home>
+    </Protected>,
   },
   {
     path: '/login',
@@ -29,19 +36,34 @@ const router = createBrowserRouter([
   },
   {
     path: '/cart',
-    element: <CartPage></CartPage>,
+    element: <Protected>
+      <CartPage></CartPage>
+    </Protected>,
   },
   {
     path: '/checkout',
-    element: <Checkout></Checkout>,
+    element: <Protected>
+      <Checkout></Checkout>
+    </Protected>,
   },
   {
-    path: '/product-detail',
-    element: <ProductDetailPage></ProductDetailPage>,
+    path: '/product-detail/:id',
+    element: <Protected>
+      <ProductDetailPage></ProductDetailPage>
+    </Protected>,
   },
 ]);
 
 function App() {
+  const dispatch = useDispatch();
+  const user = useSelector(selectLoggedInUser);
+
+  useEffect(()=>{
+    if(user){
+      dispatch(fetchItemsByUserIdAsync(user.id))
+    }
+  },[dispatch, user])
+  
   return (
     <div className="App">
       <RouterProvider router = {router} />
